afffb34926353ae8ad908e9c91fbbf23d4e5b901
talentlessDeveloper <kareemope52@gmail.com>
2023-04-08 16:49:53 +0100

    added JobListing and Resources Views to the routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,37 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LandingPage from '../views/LandingPage.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LandingPage from "../views/LandingPage.vue";
+import Resources from "../views/Resources.vue";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: LandingPage
+    path: "/",
+    name: "home",
+    component: LandingPage,
   },
   {
-    path: '/about',
-    name: 'about',
+    path: "/about",
+    name: "about",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/job-listings",
+    name: "JobListings",
+    component: () => import("../views/JobListings.vue"),
+  },
+  {
+    path: "/resources",
+    name: "Resources",
+    component: Resources,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
