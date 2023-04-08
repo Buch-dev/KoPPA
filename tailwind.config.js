f830be0ab247019e3438d72127404ea46010d76b
talentlessDeveloper <kareemope52@gmail.com>
2023-04-08 16:47:31 +0100

    added additional colors for the job listing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: ["./public/**/*.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       container: {
         center: true,
-        padding: '2rem'
+        padding: "2rem",
       },
       colors: {
-        primaryYellow: '#EDD423',
-        referBtn: '#A5CD6A',
-        signUpCorperBtn: '#079D44',
-        bannerBlendColor: '#27302C',
-        blackColor: '#001009',
-        jobCardBackgroundColor: '#FAFFFC',
-        jobCardBorderColor: '1px solid rgba(39, 48, 44, 0.1)',
-        jobCardBackgroundColorHover: '1px solid rgba(39, 48, 44, 0.2)',
-        empowerBackgroundColor: '#21513B',
-        companiesColor: '#48544C',
+        primaryYellow: "#EDD423",
+        referBtn: "#A5CD6A",
+        signUpCorperBtn: "#079D44",
+        bannerBlendColor: "#27302C",
+        blackColor: "#001009",
+        jobCardBackgroundColor: "#FAFFFC",
+        jobCardBorderColor: "1px solid rgba(39, 48, 44, 0.1)",
+        jobCardBackgroundColorHover: "1px solid rgba(39, 48, 44, 0.2)",
+        empowerBackgroundColor: "#21513B",
+        companiesColor: "#48544C",
+        jobListingsBorderColor: "#487456",
+        jobListingsColor: "#494949",
+        jobListingsApplyBtnColor: "#034f1c",
       },
       fontFamily: {
-        sans: ['Poppins', 'sans-serif']
-      }
+        sans: ["Poppins", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
