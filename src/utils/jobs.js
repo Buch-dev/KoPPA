503daf8e87a6b441a27f1d838d84b15e8746598b
talentlessDeveloper <kareemope52@gmail.com>
2023-04-08 16:51:59 +0100

    added a jobs file to hold all datas in the JobListings View

---
+++ src/utils/jobs.js
@@ -0,0 +1,179 @@
+export const recommendedJobs = [
+  {
+    logo: "inova-lab-desktop.svg",
+    mobilelogo: "inova-lab-mobile.svg",
+    heading: "Microbiology Laboratory Intern",
+    mobileinfo: " We are hiring for Microbiology Post",
+    desktopinfo: ` We are hiring social media post: The basics · Ensure the post
+            clearly states that it is a job promotion (for example, include
+            “We're hiring” or “Join our team”)`,
+    duration: "Full Time",
+    state: "Lagos",
+    companyname: "Inova",
+    timeposted: "2 days ago",
+    id: "recommendedJob320r98u38f8u81",
+  },
+  {
+    logo: "ade-and-sons-desktop.svg",
+    mobilelogo: "ade-and-sons-mobile.svg",
+    heading: "Accountant Intern",
+    mobileinfo: " We are hiring for Microbiology Post",
+    desktopinfo: ` We are hiring social media post: The basics · Ensure the post
+            clearly states that it is a job promotion (for example, include
+            “We're hiring” or “Join our team”)`,
+    duration: "Full Time",
+    state: "Lagos",
+    companyname: "Ade and Sons Limited",
+    timeposted: "2 days ago",
+    id: "recommendedJob48ur8ud98u8s81",
+  },
+  {
+    logo: "noha-dental-desktop.svg",
+    mobilelogo: "noha-dental-mobile.svg",
+    heading: "Dental Assistant",
+    mobileinfo: " We are hiring for Microbiology Post",
+    desktopinfo: ` We are hiring social media post: The basics · Ensure the post
+            clearly states that it is a job promotion (for example, include
+            “We're hiring” or “Join our team”)`,
+    duration: "Full Time",
+    state: "Lagos",
+    companyname: "Doha Dental Clinic",
+    timeposted: "2 days ago",
+    id: "recommendedJobe9ru09u09u9c",
+  },
+];
+export const latestJobs = [
+  {
+    logo: "moon-desktop.svg",
+    mobilelogo: "moon-mobile.svg",
+    heading: "Microbiology Laboratory Intern",
+    mobileinfo: " We are hiring for Microbiology Post",
+    desktopinfo: ` We are hiring social media post: The basics · Ensure the post
+            clearly states that it is a job promotion (for example, include
+            “We're hiring” or “Join our team”)`,
+    duration: "Full Time",
+    state: "Lagos",
+    companyname: "Inova",
+    timeposted: "2 days ago",
+    id: "latestJob320r98u38f8u81",
+  },
+  {
+    logo: "inova-lab-desktop.svg",
+    mobilelogo: "inova-lab-mobile.svg",
+    heading: "Dental Assistant",
+    mobileinfo: " We are hiring for Microbiology Post",
+    desktopinfo: ` We are hiring social media post: The basics · Ensure the post
+            clearly states that it is a job promotion (for example, include
+            “We're hiring” or “Join our team”)`,
+    duration: "Full Time",
+    state: "Lagos",
+    companyname: "Doha Dental Clinic",
+    timeposted: "2 days ago",
+    id: "latestJobe9ru09u09u9c",
+  },
+  {
+    logo: "zig-arrow-desktop.svg",
+    mobilelogo: "zig-arrow-mobile.svg",
+    heading: "Accountant Intern",
+    mobileinfo: " We are hiring for Microbiology Post",
+    desktopinfo: ` We are hiring social media post: The basics · Ensure the post
+            clearly states that it is a job promotion (for example, include
+            “We're hiring” or “Join our team”)`,
+    duration: "Full Time",
+    state: "Lagos",
+    companyname: "Ade and Sons Limited",
+    timeposted: "2 days ago",
+    id: "latestJob48ur8ud98u8s81",
+  },
+
+  {
+    logo: "inova-labs-love-desktop.svg",
+    mobilelogo: "inova-labs-love-mobile.svg",
+    heading: "Dental Assistant",
+    mobileinfo: " We are hiring for Microbiology Post",
+    desktopinfo: ` We are hiring social media post: The basics · Ensure the post
+            clearly states that it is a job promotion (for example, include
+            “We're hiring” or “Join our team”)`,
+    duration: "Full Time",
+    state: "Lagos",
+    companyname: "Doha Dental Clinic",
+    timeposted: "2 days ago",
+    id: "latestJobe9ru09u09u9c",
+  },
+];
+
+export const companiesMobile = [
+  {
+    name: "Eden Hospital",
+    logo: "eden-desktop.png",
+    followers: "500",
+    title: "Private Hospital in Lagos",
+    id: "companiesMobile38u8hv898983",
+  },
+  {
+    name: "Haven Clinic",
+    followers: "100",
+    logo: "haven-clinic.png",
+    title: "Private clinic in Lagos",
+    id: "companiesMobile98u489238ru283r48db",
+  },
+];
+
+export const companiesDesktop = [
+  {
+    name: "Eden Hospital",
+    logo: "eden-desktop.png",
+    followers: "500",
+    title: "Private Hospital in Lagos",
+    id: "companies38u8hv898983",
+  },
+  {
+    name: "Aiico Insurance",
+    logo: "aiico-desktop.png",
+    followers: "100",
+    title: "Insurance company in Lagos",
+    id: "companies09r8ur74y747",
+  },
+  {
+    name: "Unilever",
+    logo: "unilever-desktop.png",
+    followers: "300",
+    title: "Nutritional Production",
+    id: "companies940r8u08uf084e",
+  },
+  {
+    name: "Haven Clinic",
+    followers: "100",
+    logo: "haven-clinic.png",
+    title: "Private clinic in Lagos",
+    id: "companies98u489238ru283r48db",
+  },
+  {
+    name: "Arike Farm",
+    followers: "400",
+    logo: "inova-blue-grey-desktop.png",
+    title: "Farm in Lagos",
+    id: "companies823e89f8y7y77e",
+  },
+  {
+    name: "Rise Real Estate",
+    followers: "600",
+    logo: "inova-grey-desktop.png",
+    title: "Private Real Estate",
+    id: "companies8ue8dd7yuhahgvd",
+  },
+  {
+    name: "Mtn",
+    followers: "200",
+    logo: "mtn-desktop.png",
+    title: "Telecommunications",
+    id: "companies7eyf87dy8738y8yc0",
+  },
+  {
+    name: "GTB",
+    followers: "200",
+    logo: "gt-desktop.png",
+    title: "Commercial Bank",
+    id: "companies7eyf87dy8738y8yc0",
+  },
+];
