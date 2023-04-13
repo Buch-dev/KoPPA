ee7e51e65d628a62e64032056ba7ef6a0cbe7b43
talentlessDeveloper <kareemope52@gmail.com>
2023-04-13 16:09:21 +0100

    created signIn views for company and corper

---
+++ src/views/SignInCompany.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <SignIn
+      message="Not an employer? sign in as a corper"
+      path="signin-corper"
+    />
+  </section>
+</template>
+
+<script>
+import SignIn from "@/components/SignIn.vue";
+
+export default {
+  name: "SignInCompany",
+  components: { SignIn },
+};
+</script>
