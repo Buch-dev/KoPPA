9c1f701523774e8bc012c82845a6c5597b6e70fb
talentlessDeveloper <kareemope52@gmail.com>
2023-04-17 15:45:41 +0100

    FAQ page

---
+++ src/views/Faq.vue
@@ -0,0 +1,133 @@
+<template>
+  <section>
+    <Nav />
+    <div class="container px-[1.875rem] mt-32">
+      <h1 class="text-2xl text-center text-blackColor md:text-5xl">FAQs</h1>
+      <div
+        class="bg-jobCardBackgroundColor shadow-[0_2px_45px_rgba(7,157,68,0.1)] mt-10"
+      >
+        <div class="bg-white font-inter space-y-3 p-5 md:pt-9 md:pb-5 md:px-20">
+          <h2
+            class="text-bannerBlendColor font-semibold text-sm leading-[27px] text-left"
+          >
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            ?
+          </h2>
+          <p class="leading-6 text-sm">
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            velit sagittis nisi elementum. Ultrices et potenti netus turpis
+            magna tincidunt. Habitasse pellentesque lobortis tortor adipiscing
+            euismod a. Viverra vehicula ullamcorper pretium egestas.
+          </p>
+        </div>
+        <div
+          class="bg-darkLightBlue font-inter space-y-3 p-5 md:pt-9 md:pb-5 md:px-20"
+        >
+          <h2
+            class="text-bannerBlendColor font-semibold text-sm leading-[27px] text-left"
+          >
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            ?
+          </h2>
+          <p class="leading-6 text-sm">
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            velit sagittis nisi elementum. Ultrices et potenti netus turpis
+            magna tincidunt. Habitasse pellentesque lobortis tortor adipiscing
+            euismod a. Viverra vehicula ullamcorper pretium egestas.
+          </p>
+        </div>
+        <div class="bg-white font-inter space-y-3 p-5 md:pt-9 md:pb-5 md:px-20">
+          <h2
+            class="text-bannerBlendColor font-semibold text-sm leading-[27px] text-left"
+          >
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            ?
+          </h2>
+          <p class="leading-6 text-sm">
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            velit sagittis nisi elementum. Ultrices et potenti netus turpis
+            magna tincidunt. Habitasse pellentesque lobortis tortor adipiscing
+            euismod a. Viverra vehicula ullamcorper pretium egestas.
+          </p>
+        </div>
+        <div
+          class="bg-darkLightBlue font-inter space-y-3 p-5 md:pt-9 md:pb-5 md:px-20"
+        >
+          <h2
+            class="text-bannerBlendColor font-semibold text-sm leading-[27px] text-left"
+          >
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            ?
+          </h2>
+          <p class="leading-6 text-sm">
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            velit sagittis nisi elementum. Ultrices et potenti netus turpis
+            magna tincidunt. Habitasse pellentesque lobortis tortor adipiscing
+            euismod a. Viverra vehicula ullamcorper pretium egestas.
+          </p>
+        </div>
+        <div class="bg-white font-inter space-y-3 p-5 md:pt-9 md:pb-5 md:px-20">
+          <h2
+            class="text-bannerBlendColor font-semibold text-sm leading-[27px] text-left"
+          >
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            ?
+          </h2>
+          <p class="leading-6 text-sm">
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            velit sagittis nisi elementum. Ultrices et potenti netus turpis
+            magna tincidunt. Habitasse pellentesque lobortis tortor adipiscing
+            euismod a. Viverra vehicula ullamcorper pretium egestas.
+          </p>
+        </div>
+        <div
+          class="bg-darkLightBlue font-inter space-y-3 p-5 md:pt-9 md:pb-5 md:px-20"
+        >
+          <h2
+            class="text-bannerBlendColor font-semibold text-sm leading-[27px] text-left"
+          >
+            Lorem ipsum dolor sit amet consectetur. Sagittis est cras consequat
+            ?
+          </h2>
+          <ol class="list-decimal pl-5 md:pl-10">
+            <li class="leading-6 text-sm">
+              Lorem ipsum dolor sit amet consectetur. Sagittis est cras
+              consequat velit sagittis nisi elementum. Ultrices et potenti netus
+              turpis magna tincidunt. Habitasse pellentesque lobortis tortor
+              adipiscing euismod a. Viverra vehicula ullamcorper pretium
+              egestas.
+            </li>
+            <li class="leading-6 text-sm">
+              Lorem ipsum dolor sit amet consectetur. Sagittis est cras
+              consequat velit sagittis nisi elementum. Ultrices et potenti netus
+              turpis magna tincidunt. Habitasse pellentesque lobortis tortor
+              adipiscing euismod a. Viverra vehicula ullamcorper pretium
+              egestas.
+            </li>
+            <li class="leading-6 text-sm">
+              Lorem ipsum dolor sit amet consectetur. Sagittis est cras
+              consequat velit sagittis nisi elementum. Ultrices et potenti netus
+              turpis magna tincidunt. Habitasse pellentesque lobortis tortor
+              adipiscing euismod a. Viverra vehicula ullamcorper pretium
+              egestas.
+            </li>
+          </ol>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section>
+    <div class="container px-[1.875rem] mt-7"></div>
+  </section>
+  <section>
+    <div class="container px-[1.875rem] mt-7"></div>
+  </section>
+</template>
+
+<script>
+import Nav from "@/components/Nav.vue";
+export default {
+  name: "Faq",
+  components: { Nav },
+};
+</script>
