02b43e539bc7fa50b2e3ae7458e985c7af4db2ff
talentlessDeveloper <kareemope52@gmail.com>
2023-04-21 11:50:45 +0100

    added signOut and getSignedIn functionality

--- src/store/index.js
+++ src/store/index.js
@@ -3,20 +3,34 @@ import { createStore } from "vuex";
 const useStore = createStore({
   state: {
     user: "",
+    signedIn: false,
   },
   getters: {
     getUser(state) {
       return state.user;
     },
+    getSignedIn(state) {
+      return state.signedIn;
+    },
   },
   mutations: {
     addUser(state, payload) {
+      state.signedIn = true;
       state.user = payload;
+
+      console.log(state.signedIn);
+    },
+    signOut(state) {
+      state.signedIn = false;
+      localStorage.removeItem("koppa-token");
     },
   },
   actions: {
     addUser(context, payload) {
       context.commit("addUser", payload);
+    },
+    signOut(context) {
+      context.commit("signOut");
     },
   },
   modules: {},
