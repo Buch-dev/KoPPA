97e8989afa11bf2ca3da03f0f45e717d98a8e497
talentlessDeveloper <kareemope52@gmail.com>
2023-04-21 12:01:13 +0100

    FIX /adjusted spacings

--- src/components/Refer.vue
+++ src/components/Refer.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="bg-primaryYellow">
-    <div class="container px-[1.875rem] flex justify-between items-center py-2">
+    <div
+      class="container px-[1.875rem] xl:pl-32 flex justify-between xl:justify-normal items-center py-2"
+    >
       <p class="w-1/2 font-medium text-xs md:text-sm">{{ msg }}</p>
-      <div class="btn-close flex justify-between gap-x-3 items-center w-1/2">
+      <div class="btn-close flex gap-x-3 xl:gap-x-36 items-center w-1/2">
         <button
           class="bg-referBtn rounded-lg py-2 md:px-3 ml-5 w-28 font-medium text-xs md:w-36 md:text-base md:ml-40"
         >
