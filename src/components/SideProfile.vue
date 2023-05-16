aad8c8863db39682c512923b5b121c037269763b
talentlessDeveloper <kareemope52@gmail.com>
2023-05-16 09:23:14 +0100

    added methods and click events to none available features

--- src/components/SideProfile.vue
+++ src/components/SideProfile.vue
@@ -7,11 +7,21 @@
         <li>
           <router-link to="/user">Profile</router-link>
         </li>
-        <li>Notifications</li>
-        <li>Settings</li>
-        <li>Verification</li>
-        <li>Change Password</li>
-        <li>Referral</li>
+        <li class="cursor-pointer" @click="notificationModal = true">
+          Notifications
+        </li>
+        <li @click="handleNotAvailableModalOpen" class="cursor-pointer">
+          Settings
+        </li>
+        <li @click="handleNotAvailableModalOpen" class="cursor-pointer">
+          Verification
+        </li>
+        <li @click="handleNotAvailableModalOpen" class="cursor-pointer">
+          Change Password
+        </li>
+        <li @click="handleNotAvailableModalOpen" class="cursor-pointer">
+          Referral
+        </li>
       </ul>
     </div>
     <div class="bg-white rounded-medium p-[3.125rem]">
@@ -19,23 +29,60 @@
         <li>
           <router-link to="/" @click="logOut">log out</router-link>
         </li>
-        <li>Delete Account</li>
+        <li @click="handleNotAvailableModalOpen" class="cursor-pointer">
+          Delete Account
+        </li>
       </ul>
     </div>
   </div>
+
+  <div class="fixed">
+    <NotAvailableModal
+      v-if="notAvailable"
+      :closeModal="handleNotAvailableModalClose"
+    />
+    <div v-else></div>
+  </div>
+
+  <div class="fixed">
+    <Modal
+      v-if="notificationModal"
+      :closeModal="() => (notificationModal = false)"
+    />
+    <div v-else></div>
+  </div>
 </template>
 <script>
+import { ref } from "vue";
 import { useStore } from "vuex";
+import NotAvailableModal from "./NotAvailableModal.vue";
+import Modal from "./Modal.vue";
 export default {
   name: "SideProfile",
+  components: { NotAvailableModal, Modal },
   setup() {
     const store = useStore();
+    const notAvailable = ref(false);
+    const notificationModal = ref(false);
+
+    const handleNotAvailableModalClose = () => {
+      notAvailable.value = false;
+    };
+    const handleNotAvailableModalOpen = () => {
+      notAvailable.value = true;
+    };
 
     const logOut = () => {
       store.dispatch("signOut");
     };
 
-    return { logOut };
+    return {
+      logOut,
+      handleNotAvailableModalClose,
+      handleNotAvailableModalOpen,
+      notAvailable,
+      notificationModal,
+    };
   },
 };
 </script>
