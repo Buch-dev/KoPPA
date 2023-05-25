bd13b525ff74d3764843d8cfbd1c2ef3399aa9a8
talentlessDeveloper <kareemope52@gmail.com>
2023-05-25 11:38:13 +0100

    removed .png from url

--- src/components/CompaniesComponent.vue
+++ src/components/CompaniesComponent.vue
@@ -4,9 +4,9 @@
     :class="{ mobileCompany: !company.mobile }"
   >
     <img
-      :src="require(`../assets/img/${company.logo}.png`)"
+      :src="require(`../assets/img/${company.logo}`)"
       alt=""
-      class="w-full aspect-auto max-h-[14.125rem] object-cover rounded-medium"
+      class="w-full aspect-auto max-h-[14.25rem] object-cover rounded-medium"
     />
     <h4 class="text-sm font-medium md:text-lg mt-4">{{ company.name }}</h4>
     <p class="text-xs md:text-sm mt-4 whitespace-nowrap">{{ company.field }}</p>
